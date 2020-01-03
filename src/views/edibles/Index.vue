eb3511af6089354f0458eac353900142aa0924f0
Neah Nieto <nrpcnieto@gmail.com>
2020-01-02 19:31:15 -0600

    Add code to connect index API action to edibles/Index.view

--- src/views/edibles/Index.vue
+++ src/views/edibles/Index.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="edibles-index">
     <h1>{{ message }}</h1>
+    <div v-for="edible in edibles">
+      <div>
+        <h2>{{ edible.name }}</h2>
+        {{ edible.is_vegetarian }}
+        <br />
+        {{ edible.is_vegan }}
+        <br />
+
+        <router-link v-bind:to="`/edibles/${edible.id}`">More Info</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
