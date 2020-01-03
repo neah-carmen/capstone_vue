46d6523c2c24e87ac47d3211317f5e5d2aa79540
Neah Nieto <nrpcnieto@gmail.com>
2020-01-02 19:26:04 -0600

    Add view (sessions/Destroy.vue) and boilerplate code for logging out

---
+++ src/views/sessions/Destroy.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sessions-destroy"></div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  created: function() {
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("jwt");
+    this.$router.push("/sessions/create/");
+  }
+};
+</script>
