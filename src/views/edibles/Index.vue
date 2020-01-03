34b5c019958c5469442180e9fc00f0bea115dcce
Neah Nieto <nrpcnieto@gmail.com>
2020-01-02 18:49:09 -0600

    Generate edibles/Index.vue to match schema from backend

---
+++ src/views/edibles/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Edibles index view"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
