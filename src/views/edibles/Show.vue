6f9b536079b8aa9f4995ba5723102c776181f4e7
Neah Nieto <nrpcnieto@gmail.com>
2020-01-02 19:54:58 -0600

    Add code to connect show API action to edibles/Show.vue

--- src/views/edibles/Show.vue
+++ src/views/edibles/Show.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="edibles-show">
     <h1>{{ message }}</h1>
+    <div>
+      <h2>{{ edible.name }}</h2>
+      {{ edible.is_vegetarian }}
+      <br />
+      {{ edible.is_vegan }}
+      <br />
+      <router-link to="/edibles/">Back</router-link>
+    </div>
   </div>
 </template>
 
@@ -8,13 +16,20 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Edibles show view"
+      message: "Edibles show view",
+      edible: {}
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/edibles/" + this.$route.params.id).then(response => {
+      this.edible = response.data;
+    });
+  },
   methods: {}
 };
 </script>
