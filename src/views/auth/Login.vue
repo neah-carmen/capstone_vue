23585fed44c18c89739f55fcb4c9337e49116587
Neah Nieto <nrpcnieto@gmail.com>
2020-01-15 09:35:51 -0600

    Rearrange directory structure to mirror backend (one auth folder instead of separate sessions and users)

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="users-create">
+  <div class="auth-login">
     <div class="container">
       <form v-on:submit.prevent="submit()">
-        <h1>Signup</h1>
+        <h1>Login</h1>
         <ul>
           <li class="text-danger" v-for="error in errors">{{ error }}</li>
         </ul>
-        <div class="form-group">
-          <label>Username:</label>
-          <input type="text" class="form-control" v-model="username" />
-        </div>
         <div class="form-group">
           <label>Email:</label>
           <input type="email" class="form-control" v-model="email" />
@@ -17,14 +13,6 @@
         <div class="form-group">
           <label>Password:</label>
           <input type="password" class="form-control" v-model="password" />
-        </div>
-        <div class="form-group">
-          <label>Password confirmation:</label>
-          <input
-            type="password"
-            class="form-control"
-            v-model="passwordConfirmation"
-          />
         </div>
         <input type="submit" class="btn btn-primary" value="Submit" />
       </form>
@@ -38,28 +26,29 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      username: "",
       email: "",
       password: "",
-      passwordConfirmation: "",
       errors: []
     };
   },
   methods: {
     submit: function() {
       var params = {
-        username: this.username,
         email: this.email,
-        password: this.password,
-        password_confirmation: this.passwordConfirmation
+        password: this.password
       };
       axios
-        .post("/api/v1/auth/signup", params)
+        .post("/api/v1/auth/login", params)
         .then(response => {
-          this.$router.push("/");
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          this.$router.push("/edibles/");
         })
         .catch(error => {
-          this.errors = error.response.data.errors;
+          this.errors = ["Invalid email or password."];
+          this.email = "";
+          this.password = "";
         });
     }
   }
--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="sessions-create">
+  <div class="auth-signup">
     <div class="container">
       <form v-on:submit.prevent="submit()">
-        <h1>Login</h1>
+        <h1>Signup</h1>
         <ul>
           <li class="text-danger" v-for="error in errors">{{ error }}</li>
         </ul>
+        <div class="form-group">
+          <label>Username:</label>
+          <input type="text" class="form-control" v-model="username" />
+        </div>
         <div class="form-group">
           <label>Email:</label>
           <input type="email" class="form-control" v-model="email" />
@@ -13,6 +17,14 @@
         <div class="form-group">
           <label>Password:</label>
           <input type="password" class="form-control" v-model="password" />
+        </div>
+        <div class="form-group">
+          <label>Password confirmation:</label>
+          <input
+            type="password"
+            class="form-control"
+            v-model="passwordConfirmation"
+          />
         </div>
         <input type="submit" class="btn btn-primary" value="Submit" />
       </form>
@@ -26,29 +38,28 @@ import axios from "axios";
 export default {
   data: function() {
     return {
+      username: "",
       email: "",
       password: "",
+      passwordConfirmation: "",
       errors: []
     };
   },
   methods: {
     submit: function() {
       var params = {
+        username: this.username,
         email: this.email,
-        password: this.password
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
       };
       axios
-        .post("/api/sessions", params)
+        .post("/api/v1/auth/signup", params)
         .then(response => {
-          axios.defaults.headers.common["Authorization"] =
-            "Bearer " + response.data.jwt;
-          localStorage.setItem("jwt", response.data.jwt);
-          this.$router.push("/edibles/");
+          this.$router.push("/auth/login/");
         })
         .catch(error => {
-          this.errors = ["Invalid email or password."];
-          this.email = "";
-          this.password = "";
+          this.errors = error.response.data.errors;
         });
     }
   }
