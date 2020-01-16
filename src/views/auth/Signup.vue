73be2565a1f2c85d2a1e619ac10c92058eda420b
Neah Nieto <nrpcnieto@gmail.com>
2020-01-15 21:17:24 -0600

    Add basic styling to Auth views

--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -1,33 +1,39 @@
 <template>
   <div class="auth-signup">
     <div class="container">
-      <form v-on:submit.prevent="submit()">
-        <h1>Signup</h1>
-        <ul>
-          <li class="text-danger" v-for="error in errors">{{ error }}</li>
-        </ul>
-        <div class="form-group">
-          <label>Username:</label>
-          <input type="text" class="form-control" v-model="username" />
+      <div class="columns">
+        <div class="column is-one-fifth"></div>
+        <div class="column">
+          <form v-on:submit.prevent="submit()">
+            <h1>Signup</h1>
+            <ul>
+              <li class="text-danger" v-for="error in errors">{{ error }}</li>
+            </ul>
+            <div class="form-group">
+              <label>Username:</label>
+              <input type="text" class="form-control" v-model="username" />
+            </div>
+            <div class="form-group">
+              <label>Email:</label>
+              <input type="email" class="form-control" v-model="email" />
+            </div>
+            <div class="form-group">
+              <label>Password:</label>
+              <input type="password" class="form-control" v-model="password" />
+            </div>
+            <div class="form-group">
+              <label>Password confirmation:</label>
+              <input
+                type="password"
+                class="form-control"
+                v-model="passwordConfirmation"
+              />
+            </div>
+            <input type="submit" class="btn btn-primary" value="Submit" />
+          </form>
         </div>
-        <div class="form-group">
-          <label>Email:</label>
-          <input type="email" class="form-control" v-model="email" />
-        </div>
-        <div class="form-group">
-          <label>Password:</label>
-          <input type="password" class="form-control" v-model="password" />
-        </div>
-        <div class="form-group">
-          <label>Password confirmation:</label>
-          <input
-            type="password"
-            class="form-control"
-            v-model="passwordConfirmation"
-          />
-        </div>
-        <input type="submit" class="btn btn-primary" value="Submit" />
-      </form>
+        <div class="column is-one-fifth"></div>
+      </div>
     </div>
   </div>
 </template>
