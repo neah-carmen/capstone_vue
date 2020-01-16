430ad2ff55bc13e3e28fbef1631c4a75187c28c6
Neah Nieto <nrpcnieto@gmail.com>
2020-01-15 20:14:04 -0600

    Add image to navbar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,52 @@
 <template>
   <div id="app">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-          <a class="navbar-item">
-            <router-link to="/edibles/">Home</router-link>
-          </a>
-          <a class="navbar-item">
-            Documentation
-          </a>
-        </div>
-      </div>
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              User
-            </a>
-            <div class="navbar-dropdown">
+    <div class="columns">
+      <div class="column"></div>
+      <div class="column is-four-fifths">
+        <nav
+          class="navbar is-success"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div class="navbar-brand">
+            <img src="../src/assets/nav-image.png" height="50" width="50" />
+          </div>
+
+          <div id="navbar" class="navbar-menu">
+            <div class="navbar-start">
               <a class="navbar-item">
-                <router-link to="/auth/signup">Signup</router-link>
+                <router-link to="/edibles/">Home</router-link>
               </a>
-              <hr class="navbar-divider" />
               <a class="navbar-item">
-                <router-link to="/auth/login">Login</router-link>
+                Documentation
               </a>
-              <a class="navbar-item"
-                ><router-link to="/auth/logout">Logout</router-link></a
-              >
             </div>
           </div>
-        </div>
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                  Register
+                </a>
+                <div class="navbar-dropdown">
+                  <a class="navbar-item">
+                    <router-link to="/auth/signup">Signup</router-link>
+                  </a>
+                  <hr class="navbar-divider" />
+                  <a class="navbar-item">
+                    <router-link to="/auth/login">Login</router-link>
+                  </a>
+                  <a class="navbar-item"
+                    ><router-link to="/auth/logout">Logout</router-link></a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
-    </nav>
+      <div class="column"></div>
+    </div>
     <router-view />
   </div>
 </template>
