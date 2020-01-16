026429b5f7e1c6b8fb5053528454f2ff91321b35
Neah Nieto <nrpcnieto@gmail.com>
2020-01-15 19:02:19 -0600

    Add card component to Show.vue to display edible data

--- src/views/edibles/Show.vue
+++ src/views/edibles/Show.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="edibles-show">
-    <div>
-      <h2>{{ edible.name }}</h2>
-      <div v-for="image in images" v-bind:key="image.id">
-        <img v-bind:src="image.url" />
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image">
+              <div v-for="image in images" v-bind:key="image.id">
+                <img v-bind:src="image.url" />
+              </div>
+            </figure>
+          </div>
+        </div>
+
+        <div class="content">
+          <h2>{{ edible.name }}</h2>
+          Vegetarian: {{ edible.isVegetarian }}
+          <br />
+          Vegan: {{ edible.isVegan }}
+          <br />
+          <div v-for="ingredient in ingredients" v-bind:key="ingredient.id">
+            {{ ingredient.name }}
+          </div>
+          <br />
+          <router-link to="/edibles/">Back</router-link>
+        </div>
       </div>
-      Vegetarian: {{ edible.isVegetarian }}
-      <br />
-      Vegan: {{ edible.isVegan }}
-      <br />
-      <div v-for="ingredient in ingredients" v-bind:key="ingredient.id">
-        {{ ingredient.name }}
-      </div>
-      <router-link to="/edibles/">Back</router-link>
     </div>
   </div>
 </template>
