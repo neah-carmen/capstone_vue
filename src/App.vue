ac71d86054f0db555fc3a287d32ef6ff65fbd456
Neah Nieto <nrpcnieto@gmail.com>
2020-01-15 18:52:36 -0600

    Change navbar to use bulma components

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/edibles/">Home</router-link>|
-      <router-link to="/auth/signup">Signup</router-link>|
-      <router-link to="/auth/login">Login</router-link>|
-      <router-link to="/auth/logout">Logout</router-link>
-    </div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            <router-link to="/edibles/">Home</router-link>
+          </a>
+          <a class="navbar-item">
+            Documentation
+          </a>
+        </div>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              User
+            </a>
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                <router-link to="/auth/signup">Signup</router-link>
+              </a>
+              <hr class="navbar-divider" />
+              <a class="navbar-item">
+                <router-link to="/auth/login">Login</router-link>
+              </a>
+              <a class="navbar-item"
+                ><router-link to="/auth/logout">Logout</router-link></a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
