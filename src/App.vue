817d5f9403d41258f00a187d2cd3a994362ca5d5
Neah Nieto <nrpcnieto@gmail.com>
2020-01-19 15:17:29 -0600

    Add router link to New.vue and move navbar into its own div

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,60 @@
 <template>
   <div id="app">
-    <div class="columns">
-      <div class="column"></div>
-      <div class="column is-four-fifths">
-        <nav
-          class="navbar is-success"
-          role="navigation"
-          aria-label="main navigation"
-        >
-          <div class="navbar-brand">
-            <img src="../src/assets/nav-image.png" height="50" width="50" />
-          </div>
-
-          <div id="navbar" class="navbar-menu">
-            <div class="navbar-start">
-              <a class="navbar-item">
-                <router-link to="/edibles/">Home</router-link>
-              </a>
-              <div class="buttons">
-                <a class="button is-info">
-                  New Edible
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link">
-                  Register
-                </a>
-                <div class="navbar-dropdown">
-                  <a class="navbar-item">
-                    <router-link to="/auth/signup">
-                      <!-- <router-link to="/auth/signup" v-if="!jwt"> -->
-                      Signup</router-link
-                    >
-                  </a>
-                  <hr class="navbar-divider" />
-                  <a class="navbar-item">
-                    <router-link to="/auth/login">
-                      <!-- <router-link to="/auth/login" v-if="!jwt"> -->
-                      Login</router-link
-                    >
-                  </a>
-                  <a class="navbar-item">
-                    <router-link to="/auth/logout">
-                      <!-- <router-link to="/auth/logout" v-if="jwt"> -->
-                      Logout</router-link
-                    ></a
-                  >
-                </div>
-              </div>
-            </div>
-          </div>
-        </nav>
+    <nav
+      class="navbar is-success"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <img src="../src/assets/nav-image.png" height="48" width="48" />
       </div>
-      <div class="column"></div>
+
+      <div id="navbar" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            <router-link to="/edibles/">Home</router-link>
+          </a>
+          <div class="buttons">
+            <a class="button">
+              <router-link to="/edibles/new">New Edible</router-link>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              Register
+            </a>
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                <router-link to="/auth/signup">
+                  <!-- <router-link to="/auth/signup" v-if="!jwt"> -->
+                  Signup</router-link
+                >
+              </a>
+              <hr class="navbar-divider" />
+              <a class="navbar-item">
+                <router-link to="/auth/login">
+                  <!-- <router-link to="/auth/login" v-if="!jwt"> -->
+                  Login</router-link
+                >
+              </a>
+              <a class="navbar-item">
+                <router-link to="/auth/logout">
+                  <!-- <router-link to="/auth/logout" v-if="jwt"> -->
+                  Logout</router-link
+                ></a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 <script>
