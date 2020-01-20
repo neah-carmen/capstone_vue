b1876fa3144bc104ec8abf459606d15cba39dee4
Neah Nieto <nrpcnieto@gmail.com>
2020-01-19 23:38:20 -0600

    Add section for edit/updation action in Show.vue

--- src/views/edibles/Show.vue
+++ src/views/edibles/Show.vue
@@ -5,11 +5,6 @@
       <div class="column is-three-quarters">
         <div class="card">
           <div class="card-content">
-            <header class="card-header">
-              <p class="card-header-title">
-                {{ edible.name }}
-              </p>
-            </header>
             <div class="media">
               <div class="media-left">
                 <figure class="image">
@@ -18,22 +13,68 @@
                   </div>
                 </figure>
               </div>
+              <div class="media-content">
+                <p class="title is-4">{{ edible.name }}</p>
+                Vegetarian: {{ edible.is_vegetarian }}
+                <br />
+                Vegan: {{ edible.is_vegan }}
+                <br />
+                Ingredients:
+                <span
+                  v-for="(ingredient, index) in ingredients"
+                  v-bind:key="ingredient.id"
+                >
+                  <span>{{ ingredient.name }}</span
+                  ><span v-if="index + 1 < ingredients.length">, </span>
+                </span>
+              </div>
             </div>
             <div class="content">
-              Vegetarian: {{ edible.isVegetarian }}
-              <br />
-              Vegan: {{ edible.isVegan }}
-              <br />
-              Ingredients:
-              <span
-                v-for="(ingredient, index) in ingredients"
-                v-bind:key="ingredient.id"
-              >
-                <span>{{ ingredient.name }}</span
-                ><span v-if="index + 1 < ingredients.length">, </span>
-              </span>
               <footer class="card-footer">
-                <router-link to="/edibles/">Back</router-link>
+                <div class="content">
+                  <button v-on:click="toggleEdibleEditModal(edible)">
+                    Edit Edible
+                  </button>
+
+                  <div
+                    v-bind:class="{ selected: edible === selectedEdible }"
+                    v-if="edible === selectedEdible"
+                  >
+                    <br />Name:
+                    <input type="text" v-model="edible.name" />
+                    <br />UPC:
+                    <input type="text" v-model="edible.upc" />
+                    <br />
+                    Vegetarian: {{ edible.is_vegetarian }}
+                    <br />
+                    Vegan: {{ edible.is_vegan }}
+                    <br />
+                    <button v-on:click="checkEdible(edible)">
+                      Check Edible
+                    </button>
+                    <br />
+                    Ingredients:
+                    <div
+                      v-for="ingredient in ingredients"
+                      v-bind:key="ingredient.id"
+                    >
+                      <input type="text" v-model="ingredient.name" />
+                      <select v-model="ingredient.is_vegetarian">
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                      </select>
+                      <select v-model="ingredient.is_vegan">
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                      </select>
+                    </div>
+                    <button v-on:click="updateEdible(edible)">
+                      Update Edible
+                    </button>
+                  </div>
+                  <br />
+                  <router-link to="/edibles/">Back</router-link>
+                </div>
               </footer>
             </div>
           </div>
@@ -53,17 +94,26 @@ export default {
   data: function() {
     return {
       edible: {},
+      images: {},
       ingredients: {},
-      images: {}
+      selectedEdible: {}
     };
   },
   created: function() {
-    axios.get("/api/v1/edibles/" + this.$route.params.id).then(response => {
+    axios.get("/api/edibles/" + this.$route.params.id).then(response => {
       this.edible = response.data;
-      this.ingredients = response.data.Ingredients;
-      this.images = response.data.LabelImages;
+      this.images = response.data.images;
+      this.ingredients = response.data.ingredients;
     });
   },
-  methods: {}
+  methods: {
+    toggleEdibleEditModal: function(edible) {
+      if (this.selectedEdible === edible) {
+        this.selectedEdible = {};
+      } else {
+        this.selectedEdible = edible;
+      }
+    }
+  }
 };
 </script>
